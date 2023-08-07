41c1599aa2146f60f270e613f37a961dfa151986
Zakery Kates <zakkates@simple-edge.com>
2023-08-07 11:09:39 -0500

    Changed production directory to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
 	mode: "development", // Or 'production'
@@ -18,5 +19,9 @@ module.exports = {
 			},
 			// other rules...
 		],
+	},
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: "main.js",
 	},
 };
